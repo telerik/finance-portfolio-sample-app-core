5c9c464b698225f04f2bfe0b8604181ca9f5bad0
Tsvetomir Milchev <tmilchev@progress.com>
2019-11-21 16:00:12 +0200

    chore: add default serializer

--- FinancePortfolio/FinancePortfolio/Startup.cs
+++ FinancePortfolio/FinancePortfolio/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
 
 namespace FinancePortfolio
 {
@@ -23,6 +24,11 @@ namespace FinancePortfolio
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc()
+                    .AddNewtonsoftJson(options =>
+                            options.SerializerSettings.ContractResolver =
+                                new DefaultContractResolver());
+
             services.AddKendo();
             services.AddControllersWithViews();
         }
