8ca80aded5b8040eaa7cb5c0e014371b0a0d66c6
Nikolay Penev <npenev@progress.com>
2019-12-05 18:35:41 +0200

    add currency and color change for bttmpane grid

--- FinancePortfolio/FinancePortfolio/wwwroot/js/site.js
+++ FinancePortfolio/FinancePortfolio/wwwroot/js/site.js
@@ -4,8 +4,13 @@
 // Write your JavaScript code.
 const symbols = { USD: "$", EUR: "€", GBP: "£" }
 
-function onChange(e) {
-    $("#stocksGrid").getKendoGrid().dataSource.read();
+function onCurrencyChange(e) {
+    if (document.location.pathname == "/Home/DataVirtualization") {
+        $("#stocksGrid").getKendoGrid().dataSource.read();
+    }
+    if (document.location.pathname == "/") {
+        $("#Grid").getKendoGrid().dataSource.read();
+    }
 }
 
 function additionalData(e) {
@@ -25,6 +30,20 @@ function priceTemplate(dataItem) {
     return "<span style='color: " + color + "'>" + currencySymbol + ' ' + kendo.toString(dataItem.Price, "0.00") + "</span>";
 }
 
+function currencyTemplate(data) {
+    var currency = $("#currency").data("kendoDropDownList").value();
+    return currency == 1 ? kendo.toString(data.Price, "$###.##") : (currency == 2 ? kendo.toString(data.Price, "€###.##") : kendo.toString(data.Price, "£###.##"))
+}
+
+function dayChangeTemplate(data) {
+    var color = data.DayChange == 0 ? "none" : (data.DayChange > 0 ? "green" : "red");
+    return "<span style='color: " + color + "'>" + kendo.toString(data.DayChange, "0.00") + "</span>";
+}
+function dayChangePctTemplate(data) {
+    var color = data.DayChange == 0 ? "none" : (data.DayChange > 0 ? "green" : "red");
+    return "<span style='color: " + color + "'>" + kendo.toString(data.DayChange, "0.00") + "%</span>";
+}
+
 function changeTemplate(dataItem) {
     var color = dataItem.DayChange == 0 ? "none" : (dataItem.Price > 0 ? "green" : "red");
 
@@ -32,7 +51,6 @@ function changeTemplate(dataItem) {
 }
 
 function additionalChartData() {
-    debugger
     return {
         symbol: "AAN",
         start: new Date(2019, 7, 10).toUTCString(),
