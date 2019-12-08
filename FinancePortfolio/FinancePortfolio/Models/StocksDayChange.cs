640a6292fdead1bf57f85354c51d34dd28dc319e
Petar Todorov <petar.todorov@progress.com>
2019-12-08 23:20:10 +0200

    Fix Heatmap. Fix Profile layout. Fix general layout

---
+++ FinancePortfolio/FinancePortfolio/Models/StocksDayChange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancePortfolio.Models
+{
+    public class StocksDayChange
+    {
+        public StocksDayChange(string name, decimal value, List<StocksDayChange> items,string symbol, decimal change)
+        {
+            Name = name;
+            Symbol = symbol;
+            Change = change;
+            Value = value;
+            Items = items;
+        }
+
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+
+        public decimal Value { get; set; }
+        public decimal Change { get; set; }
+        public List<StocksDayChange> Items { get; set; }
+    }
+}
