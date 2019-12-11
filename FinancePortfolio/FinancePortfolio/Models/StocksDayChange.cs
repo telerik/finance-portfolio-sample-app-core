6c9bc76a928d66decdcfe929147a8e4f4553fc2c
Petar Todorov <petar.todorov@progress.com>
2019-12-11 09:10:41 +0200

    Add heatmap ToolTip. Fix design issues

--- FinancePortfolio/FinancePortfolio/Models/StocksDayChange.cs
+++ FinancePortfolio/FinancePortfolio/Models/StocksDayChange.cs
@@ -7,20 +7,22 @@ namespace FinancePortfolio.Models
 {
     public class StocksDayChange
     {
-        public StocksDayChange(string name, decimal value, List<StocksDayChange> items,string symbol, decimal change)
+        public StocksDayChange(string name, decimal value, List<StocksDayChange> items,string symbol, decimal change, decimal price)
         {
             Name = name;
             Symbol = symbol;
             Change = change;
             Value = value;
             Items = items;
+            Price = price;
         }
 
         public string Name { get; set; }
         public string Symbol { get; set; }
-
         public decimal Value { get; set; }
         public decimal Change { get; set; }
+        public decimal Price { get; set; }
+
         public List<StocksDayChange> Items { get; set; }
     }
 }
